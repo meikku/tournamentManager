79dd86a33c4dbdac479f8ae430edf89f896cfce0
theray1 <theray1@outlook.fr>
2022-11-01 16:21:30 +0100

    Added tests for addPreviousGame and Finish methods from Game Class

--- src/test/java/tournamentmanager/core/GameTest.java
+++ src/test/java/tournamentmanager/core/GameTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tournamentmanager.core.api.Game;
 import tournamentmanager.core.api.Participant;
+import tournamentmanager.core.api.Status;
 import tournamentmanager.core.api.TournamentException;
 import tournamentmanager.core.impl.GameImpl;
 import tournamentmanager.core.impl.ParticipantImpl;
@@ -105,5 +106,103 @@ public class GameTest {
     }
 
     //addPreviousGame
+    @Test
+    void addPreviousGameProperlyAddsAPreviousGameWhenItIsNotAlreadyAddedAndPreviousGamesContainsLessThanTwoGames(){
+        Game gameToAdd = new GameImpl();
 
+        assertDoesNotThrow(() -> g.addPreviousGame(gameToAdd));
+
+        List<Game> expected = new ArrayList<>();
+        expected.add(gameToAdd);
+
+        assertIterableEquals(expected, g.getPreviousGames());
+    }
+
+    @Test
+    void addPreviousGameThrowsTournamentExceptionWhenTheGameToAddHasAlreadyBeenAdded(){
+        Game gameToAdd = new GameImpl();
+
+        assertDoesNotThrow(() -> g.addPreviousGame(gameToAdd));
+
+        assertThrows(TournamentException.class,
+                () -> g.addPreviousGame(gameToAdd));
+    }
+
+    @Test
+    void addPreviousGameThrowsTournamentExceptionWhenPreviousGamesAlreadyContainsTwoOrMoreGames(){
+        Game fillerGame1 = new GameImpl();
+        Game fillerGame2 = new GameImpl();
+        Game gameToAdd = new GameImpl();
+
+        assertDoesNotThrow(() -> g.addPreviousGame(fillerGame1));
+        assertDoesNotThrow(() -> g.addPreviousGame(fillerGame2));
+
+        assertThrows(TournamentException.class,
+                () -> g.addPreviousGame(gameToAdd));
+    }
+
+    @Test
+    void addPreviousGameThrowsTournamentExceptionWhenGameIsNotStarted(){
+        Game gameToAdd = new GameImpl();
+
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertThrows(TournamentException.class,
+                () -> g.addPreviousGame(gameToAdd));
+    }
+
+    @Test
+    void addPreviousGameThrowsIllegalArgumentExceptionWhenTheGameToAddIsNull(){
+        Game gameToAdd = null;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> g.addPreviousGame(gameToAdd));
+    }
+    
+    //finish
+    @Test
+    void finishThrowsTournamentExceptionWhenScoresAreEven(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertDoesNotThrow(() -> g.addPoints(p1, 1));
+        assertDoesNotThrow(() -> g.addPoints(p2, 1));
+
+        assertThrows(TournamentException.class,
+                () -> g.finish());
+    }
+
+    @Test
+    void finishThrowsTournamentExceptionWhenGameIsNotInProgress(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertThrows(TournamentException.class,
+                () -> g.finish());
+    }
+
+    @Test
+    void finishProperlyFinishesTheGame(){
+        Game followingGame = new GameImpl();
+        g.setFollowingGame(followingGame);
+
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertDoesNotThrow(() -> g.addPoints(p1, 1));
+        assertDoesNotThrow(() -> g.addPoints(p2, 2));
+
+        assertDoesNotThrow(() -> g.finish());
+
+        assertEquals(Status.FINISHED, g.getStatus());
+        assertTrue(g.getFollowingGame().get().getParticipants().contains(p2));
+        assertTrue(p1.isEliminated());
+    }
 }
