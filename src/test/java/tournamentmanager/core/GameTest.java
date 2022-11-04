74218cd64d0216556916c37abbe545d2ad7d19e3
theray1 <theray1@outlook.fr>
2022-11-04 23:53:07 +0100

    Added tests for remaining methods from Game class

--- src/test/java/tournamentmanager/core/GameTest.java
+++ src/test/java/tournamentmanager/core/GameTest.java
@@ -10,6 +10,7 @@ import tournamentmanager.core.impl.ParticipantImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -204,5 +205,227 @@ public class GameTest {
         assertEquals(Status.FINISHED, g.getStatus());
         assertTrue(g.getFollowingGame().get().getParticipants().contains(p2));
         assertTrue(p1.isEliminated());
+    }
+
+    //start
+    @Test
+    void startThrowsTournamentExceptionWhenTheGameIsInProgressAndThereAreTwoParticipants(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertThrows(TournamentException.class,
+                () -> g.start());
+
+    }
+
+    @Test
+    void startThrowsTournamentExceptionWhenTheGameIsNotStartedAndThereAreLessThanTwoParticipants(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+
+        assertThrows(TournamentException.class,
+                () -> g.start());
+    }
+
+    @Test
+    void startSetsTheGameStatusToInProgressWhenThereAreTwoParticipantsAndTheGameIsNotStarted(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertEquals(Status.INPROGRESS, g.getStatus());
+    }
+
+    //setPreviousGame
+    @Test
+    void setFollowingGameThrowsTournamentExceptionWhenFollowingGameIsNull(){
+        assertThrows(IllegalArgumentException.class,
+                () -> g.setFollowingGame(null));
+    }
+
+    @Test
+    void setFollowingGameProperlyAssociatesGameAndFollowingGame(){
+        Game followingGameToAdd = new GameImpl();
+        g.setFollowingGame(followingGameToAdd);
+
+        Optional<Game> expected = Optional.of(followingGameToAdd);
+
+        assertEquals(expected,
+                g.getFollowingGame());
+    }
+
+    //getPreviousGames
+    @Test
+    void getPreviousGamesContainsExactlyAllPreviousGamesOfCurrentGame(){
+        Game previousGameToAdd = new GameImpl();
+        assertDoesNotThrow(() -> g.addPreviousGame(previousGameToAdd));
+
+        List<Game> expected = new ArrayList<>();
+        expected.add(previousGameToAdd);
+
+        assertIterableEquals(expected, g.getPreviousGames());
+    }
+    @Test
+    void getPreviousGamesReturnsAnEmptyListWhenThereAreNoPreviousGames(){
+        assertIterableEquals(new ArrayList<>(), g.getPreviousGames());
+    }
+
+    //getWinner
+    @Test
+    void getWinnerReturnsTheWinnerOfAGameWhenThereIsAWinnerAndTheGameIsFinished(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertDoesNotThrow(() -> g.addPoints(p1, 2));
+        assertDoesNotThrow(() -> g.addPoints(p2, 1));
+
+        assertDoesNotThrow(() -> g.finish());
+
+        try {
+            assertEquals(p1, g.getWinner());
+        } catch (TournamentException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void getWinnerThrowsTournamentExceptionWhenScoresAreNotEvenButTheGameIsNotFinished(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertDoesNotThrow(() -> g.addPoints(p1, 2));
+        assertDoesNotThrow(() -> g.addPoints(p2, 1));
+
+        assertThrows(TournamentException.class,
+                () -> g.getWinner());
+    }
+
+    @Test
+    void getWinnerThrowsTournamentExceptionWhenScoresAreEvenAndTheGameIsNotFinished(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertDoesNotThrow(() -> g.addPoints(p1, 2));
+        assertDoesNotThrow(() -> g.addPoints(p2, 2));
+
+        assertThrows(TournamentException.class,
+                () -> g.getWinner());
+    }
+
+    //getLoser
+    @Test
+    void getLoserReturnsTheWinnerOfAGameWhenThereIsAWinnerAndTheGameIsFinished(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertDoesNotThrow(() -> g.addPoints(p1, 2));
+        assertDoesNotThrow(() -> g.addPoints(p2, 1));
+
+        assertDoesNotThrow(() -> g.finish());
+
+        try {
+            assertEquals(p2, g.getLoser());
+        } catch (TournamentException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void getLoserThrowsTournamentExceptionWhenScoresAreNotEvenButTheGameIsNotFinished(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertDoesNotThrow(() -> g.addPoints(p1, 2));
+        assertDoesNotThrow(() -> g.addPoints(p2, 1));
+
+        assertThrows(TournamentException.class,
+                () -> g.getLoser());
+    }
+
+    @Test
+    void getLoserThrowsTournamentExceptionWhenScoresAreEvenAndTheGameIsNotFinished(){
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertDoesNotThrow(() -> g.addPoints(p1, 2));
+        assertDoesNotThrow(() -> g.addPoints(p2, 2));
+
+        assertThrows(TournamentException.class,
+                () -> g.getLoser());
+    }
+
+    //getStatus
+    @Test
+    void getStatusReturnsTheStatusOfTheGame(){
+        assertEquals(Status.NOTSTARTED, g.getStatus());
+
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+
+        assertDoesNotThrow(() -> g.start());
+
+        assertEquals(Status.INPROGRESS, g.getStatus());
+
+        assertDoesNotThrow(() -> g.addPoints(p1, 2));
+        assertDoesNotThrow(() -> g.addPoints(p2, 1));
+
+        assertDoesNotThrow(() -> g.finish());
+
+        assertEquals(Status.FINISHED, g.getStatus());
+    }
+
+    //getStatus
+    @Test
+    void getParticipantsReturnsExactlyAllTheParticipantsOfTheGame(){
+        List<Participant> expected = new ArrayList<>();
+
+        // compares the content of arrays which are not necessarily ordered in the same way but should contain the same elements
+        assertTrue(expected.containsAll(g.getParticipants()) && expected.size() == g.getParticipants().size());
+        assertTrue(g.getParticipants().size() <= 2);
+
+        assertDoesNotThrow(() -> g.addParticipant(p1));
+        expected.add(p1);
+
+        assertTrue(expected.containsAll(g.getParticipants()) && expected.size() == g.getParticipants().size());
+        assertTrue(g.getParticipants().size() <= 2);
+
+        assertDoesNotThrow(() -> g.addParticipant(p2));
+        expected.add(p2);
+
+        assertTrue(expected.containsAll(g.getParticipants()) && expected.size() == g.getParticipants().size());
+        assertTrue(g.getParticipants().size() <= 2);
+
+    }
+
+    //getFollowingGame
+    @Test
+    void getFollowingGameReturnsAnEmptyOptionalIfThereIsNoFollowingGame(){
+        Optional<Game> expected = Optional.empty();
+
+        assertEquals(expected, g.getFollowingGame());
+    }
+
+    @Test
+    void getFollowingGameReturnsTheFollowingGame(){
+        Game followingGameToAdd = new GameImpl();
+        Optional<Game> expected = Optional.of(followingGameToAdd);
+
+        g.setFollowingGame(followingGameToAdd);
+
+        assertEquals(expected, g.getFollowingGame());
     }
 }
